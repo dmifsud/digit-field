bf88262ff13ccf861fb6d8a7d84ac5fb2628915e
David Mifsud <david.mifsud89@gmail.com>
2015-07-17 11:12:11 +0200

    0.9.0

---
+++ build/scripts/digit-field.js
@@ -0,0 +1,45 @@
+angular.module('digit-field-template', []);
+angular.module('digit-field',['digit-field-template'])
+.directive('digitField', function($filter){
+  'use strict';
+    return {
+      restrict: 'E',
+      templateUrl: 'digit-field.html',
+      require: 'ngModel',
+      scope: {
+        actualModel: '=ngModel'
+      },
+      link: function(scope, element, attr){
+        // if (attr.class){
+        //
+        // }
+
+        //Init
+        //----------------------------------------------------------------------
+        scope.placeholder = '0.00';
+
+
+        //Watches
+        //----------------------------------------------------------------------
+
+        scope.$watch('model', function(val, old){
+
+          if (angular.isDefined(val) && val !== old){
+
+            scope.actualModel = parseInt(val || 0)/100;
+            scope.placeholder = $filter('number')(scope.actualModel, 2);
+
+
+            if (val !== null && val.length > 2){
+              scope.result = $filter('number')(scope.actualModel, 2);
+            }else{
+              scope.result = val || undefined;
+            }
+
+          }
+
+        });
+
+      }
+    };
+});
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')({pattern: ['gulp-*']}),
+    packagejson = require('./package.json'),
+    runSequence = require('run-sequence'),
+    addStream = require('add-stream'),
+    angularTemplateCache = require('gulp-angular-templatecache');
+
+var prepareTemplates = function(){
+  return gulp.src('src/templates/**/*.html')
+    .pipe(angularTemplateCache());
+};
+
+gulp.task('js', function(){
+  var minifiedVersion = "digit.field."+packagejson.version+".min.js";
+
+  return gulp.src('src/*.js')
+    //.pipe(ng annotate, minify, etc)
+    .pipe(gulp.dest('build/scripts'))
+  .pipe($.jshint())
+  .pipe($.jshint.reporter('jshint-stylish'))
+  .pipe($.jshint.reporter('fail'))
+  .pipe($.uglify({
+            mangle : false,
+            preserveComments : "some"
+        }))
+  .pipe($.concat(minifiedVersion))
+  .pipe(addStream.obj(prepareTemplates()))
+    .pipe($.concat(minifiedVersion))
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('less', function () {
+    return gulp.src('src/less/digit-field.less')
+        .pipe($.less({compress: true}))
+        .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('clean', function(){
+  return gulp.src('dist', {read: false})
+        .pipe($.clean());
+});
+
+gulp.task('dist', ['js','less']);
+
+gulp.task('default', function(){
+  runSequence('clean', ['dist']);
+});
